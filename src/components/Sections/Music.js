79b59799a6379efe9dd8738cdd36b84ac2d52851
John Ma <jpaulma23@gmail.com>
2024-03-04 15:56:48 -0500

    spotify iframe bug fix

--- src/components/Sections/Music.js
+++ src/components/Sections/Music.js
@@ -6,7 +6,7 @@ function Music() {
         <div className="section">
             <header className="stdHeader"> What I'm currently Listening To :) </header>
             <p className="sectionText"> I really love listening to music, and especially new genres, here is what I currently enjoy </p>
-            <iframe title="vibing... :)"  src="https://open.spotify.com/embed/playlist/26FhFIaZ0W7rCBPKs47jvC?utm_source=generator" width="100%" height="350" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <iframe className="spotify_iframe" title="vibing... :)"  src="https://open.spotify.com/embed/playlist/26FhFIaZ0W7rCBPKs47jvC?utm_source=generator" width="100%" height="350" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             
         </div>
         <div className="section">
