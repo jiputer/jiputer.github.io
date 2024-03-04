699599f57c6fc23dfafa738692244603abd67a54
John Ma <jpaulma23@gmail.com>
2024-03-04 16:56:06 -0500

    responsive bug spotify embedded fix

--- src/components/Sections/Music.js
+++ src/components/Sections/Music.js
@@ -5,13 +5,14 @@ function Music() {
     <div name="music" id="music" className = "sections"> 
         <div className="section">
             <header className="stdHeader"> What I'm currently Listening To :) </header>
-            <p className="sectionText"> I really love listening to music, and especially new genres, here is what I currently enjoy </p>
-            <iframe className="spotify_iframe" title="vibing... :)"  src="https://open.spotify.com/embed/playlist/26FhFIaZ0W7rCBPKs47jvC?utm_source=generator" width="100%" height="350" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            
-        </div>
-        <div className="section">
-        <img src={flower2} alt=":) ..." />
+            <p className="sectionText"> I really love listening to music, and especially new genres, 
+            here is what I currently enjoy </p>
 
+            <iframe className="spotify_iframe" title="vibing... :)"  src="https://open.spotify.com/embed/playlist/26FhFIaZ0W7rCBPKs47jvC?utm_source=generator"  frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+
+        <div className="section">
+        <img src={flower2} alt=":) ..." width="100%" height="100%"/>
         </div>
     </div>
 
